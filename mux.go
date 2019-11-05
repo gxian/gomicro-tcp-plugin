dc6476389fb35d9f4c0ab2bf6acf570e58675baa
gaoxiang <gaoxiang.ian@gmail.com>
2019-11-05 18:19:34 +0800

    断包处理完成

--- mux.go
+++ mux.go
@@ -1,6 +1,10 @@
 package main
 
-import "sync"
+import (
+	"bytes"
+	"io"
+	"sync"
+)
 
 // Encoder ...
 type Encoder interface {
@@ -9,7 +13,7 @@ type Encoder interface {
 
 // Decoder ...
 type Decoder interface {
-	Decode([]byte) (Message, error)
+	Decode([]byte) (int, Message, error)
 }
 
 // Codec ...
@@ -18,9 +22,17 @@ type Codec interface {
 	Decoder
 }
 
+// SendFunc ...
+type SendFunc func(Message) error
+
+// Send ...
+func (s SendFunc) Send(m Message) error {
+	return s(m)
+}
+
 // Sender ...
 type Sender interface {
-	Send(Message)
+	Send(Message) error
 }
 
 // Handler ...
@@ -46,6 +58,7 @@ type Message interface {
 type Multiplexer struct {
 	codec    Codec
 	handlers map[int]Handler
+	bufs     map[io.Writer]*bytes.Buffer
 	mu       *sync.Mutex
 }
 
@@ -54,22 +67,70 @@ func NewMultiplexer(c Codec) *Multiplexer {
 	return &Multiplexer{
 		codec:    c,
 		handlers: make(map[int]Handler),
+		bufs:     make(map[io.Writer]*bytes.Buffer),
 		mu:       &sync.Mutex{},
+	}
+}
+
+func (m *Multiplexer) wrapSender(w io.Writer) SendFunc {
+	return func(message Message) error {
+		b, err := m.codec.Encode(message)
+		if err != nil {
+			return err
+		}
+		_, err = w.Write(b)
+		return err
 	}
 }
 
 // HandleFunc ...
 func (m *Multiplexer) HandleFunc(id int, h Handler) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
 	m.handlers[id] = h
 }
 
 // Read ...
-func (m *Multiplexer) Read(b []byte) (int, error) {
+func (m *Multiplexer) Read(w io.Writer, b []byte) (int, error) {
 	// 断包处理
+	m.mu.Lock()
+	buf, ok := m.bufs[w]
+	if !ok {
+		buf = bytes.NewBuffer([]byte{})
+		m.bufs[w] = buf
+	}
+	m.mu.Unlock()
+
+	n, err := buf.Write(b)
+	if err != nil {
+		return n, err
+	}
+
+	n, msg, err := m.codec.Decode(buf.Bytes())
+	if err != nil {
+		// kick
+		return n, err
+	}
+
+	if n != 0 {
+		// 包体完整
+		m.mu.Lock()
+		defer m.mu.Unlock()
+		h, ok := m.handlers[int(msg.Header().MsgID())]
+		if !ok {
+			return n, nil
+		}
+		h.Handle(msg, m.wrapSender(w))
+
+		// 偏移buf
+		buf.Next(n)
+	}
+
 	return 0, nil
 }
 
 // Close ...
-func (m *Multiplexer) Close() error {
+func (m *Multiplexer) Close(w io.Writer) error {
+	// clear writer buf
 	return nil
 }
